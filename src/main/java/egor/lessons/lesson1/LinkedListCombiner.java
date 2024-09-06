ba8485557e530d6e6bca64b0df3a77fa2ec73286
Egor Pozdnov <pozdner98@gmail.com>
2024-09-06 14:21:13 +0300

    Fix combiner

--- src/main/java/egor/lessons/lesson1/LinkedListCombiner.java
+++ src/main/java/egor/lessons/lesson1/LinkedListCombiner.java
@@ -5,15 +5,17 @@ public class LinkedListCombiner {
     public static LinkedList sumLinkedListValues(LinkedList first, LinkedList second) {
         LinkedList resultList = new LinkedList();
 
-        if (first.count() == second.count()) {
-            Node firstNode = first.head;
-            Node secondNode = second.head;
+        if (first.count() != second.count()) {
+            return resultList;
+        }
 
-            while (firstNode != null) {
-                resultList.addInTail(new Node(firstNode.value + secondNode.value));
-                firstNode = firstNode.next;
-                secondNode = secondNode.next;
-            }
+        Node firstNode = first.head;
+        Node secondNode = second.head;
+
+        while (firstNode != null) {
+            resultList.addInTail(new Node(firstNode.value + secondNode.value));
+            firstNode = firstNode.next;
+            secondNode = secondNode.next;
         }
 
         return resultList;
